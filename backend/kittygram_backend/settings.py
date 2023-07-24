000d2b2c6f035e4a3f45419f7deda5241e63a83c
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-07-24 15:36:12 +0300

    Media

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.252.140.107', '127.0.0.1', 'localhost', 'prettykittygram.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
