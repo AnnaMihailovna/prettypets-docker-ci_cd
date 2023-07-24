1d8259f45ae0da676a37a0b6e600552510137c65
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-07-24 16:35:31 +0300

    Media

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -99,7 +99,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'/media'
+MEDIA_ROOT = '/var/www/kittygram/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
