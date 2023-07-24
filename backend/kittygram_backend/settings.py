19798c1c50ac63529f52b0a4aa03f5b94f6a4c30
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-07-24 18:13:36 +0300

    SECTET_KEY

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,12 +1,14 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+load_dotenv()
+SECRET_KEY = os.getenv('TOKEN', 'SECRET')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['84.252.140.107', '127.0.0.1', 'localhost', 'prettykittygram.hopto.org']
 
